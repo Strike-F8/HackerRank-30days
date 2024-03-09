647a4137da97b75f64dfd9f3dab3050593fe49ff
Edward Fernandez <Strike_F8@pm.me>
2024-03-09 19:49:02 +0900

    Completed Day3 in C#

---
+++ C#/Day3/Program.cs
@@ -0,0 +1,17 @@
+﻿class Solution
+{
+    public static void Main(String[] args)
+    {
+	int N = Convert.ToInt32(Console.ReadLine().Trim());
+	// Check if the input is weird
+	
+	if (N%2 == 1)
+	    Console.WriteLine("Weird");
+	else if(N >= 2 && N <= 5)
+	    Console.WriteLine("Not Weird");
+	else if(N >= 6 && N <= 20)
+	    Console.WriteLine("Weird");
+	else if(N > 20)
+	    Console.WriteLine("Not Weird");
+    }
+}
