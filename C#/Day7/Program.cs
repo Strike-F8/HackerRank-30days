cce7b03f9f14fb930fe6b30f877180eec478d709
Edward Fernandez <Strike_F8@pm.me>
2024-03-17 13:55:51 +0900

    Completed Day 7 in C#

---
+++ C#/Day7/Program.cs
@@ -0,0 +1,18 @@
+﻿class Solution
+{
+    public static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+	// Reverse the elements of the array
+	arr.Reverse();
+	
+	// Print out the elements of the array
+	Console.Out.NewLine = " ";
+	foreach (int i in arr)
+	{
+	    Console.WriteLine(i);
+	}
+    }
+}
