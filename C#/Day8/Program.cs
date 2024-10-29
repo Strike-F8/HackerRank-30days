109dec5eda47251eba9c1dc9bb00bb6963485b14
Edward Fernandez <Strike_F8@pm.me>
2024-10-29 20:21:13 +0900

    Solved day 8 in C#

--- C#/Day8/Program.cs
+++ C#/Day8/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Collections.Generic;
+class Solution
+{
+    public static void Main(string[] args)
+    {
+	// Get the number of entries in the phonebook
+	int num_entries = Convert.ToInt32(Console.ReadLine().Trim());
+	
+	Dictionary<string, int> phone_book = new Dictionary<string, int>();
+	string input = "";
+
+	// Get the entries
+	for (int i = 0; i < num_entries; i++)
+	{
+	    input = Console.ReadLine().Trim();
+	    // split the key and value
+	    phone_book.Add(input.Split()[0], Convert.ToInt32(input.Split()[1]));
+	}
+
+	// look up phone numbers until no more input
+	while((input = Console.ReadLine()) != null)
+	{
+	    if (phone_book.TryGetValue(input, out int number))
+		Console.WriteLine($"{input}={number}");
+	    else
+		Console.WriteLine("Not found");
+	}
+    }
+}
